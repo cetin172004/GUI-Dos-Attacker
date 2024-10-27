ef979827668585194829c4b655c09341370e6419
Ahmet Mustafa ÇETİN <cetin172004@gmail.com>
2024-10-27 05:28:38 +0200

    scan button

--- Main.java
+++ Main.java
@@ -73,17 +73,17 @@ public class Main extends Application {
         
         // (VB) Wifi Adapter & Wireless Network & Deauth Area
         
-        String[] adapter_items = { "ec1", "ec2", "ec3" };
+        String[] adapter_items = { "Please scan first" };
         ComboBox<String> wifi_adapter_cb = new ComboBox<>();
         wifi_adapter_cb.getItems().addAll(adapter_items);
-        wifi_adapter_cb.setValue("ec1");
+        wifi_adapter_cb.setValue("Please scan first");
         wifi_adapter_cb.setStyle("-fx-font-family: '" + font1.getName() + "'; -fx-font-size: 20px; -fx-background-radius: 50; -fx-background-color: white;");
         wifi_adapter_cb.setPrefWidth(300);
         
-        String[] wifi_items = { "net1", "net2", "net3" };
+        String[] wifi_items = { "Please scan first" };
         ComboBox<String> wireless_network_cb = new ComboBox<>();
         wireless_network_cb.getItems().addAll(wifi_items);
-        wireless_network_cb.setValue("net1");
+        wireless_network_cb.setValue("Please scan first");
         wireless_network_cb.setStyle("-fx-font-family: '" + font1.getName() + "'; -fx-font-size: 20px; -fx-background-radius: 50; -fx-background-color: white;");
         wireless_network_cb.setPrefWidth(300);
         
@@ -95,16 +95,22 @@ public class Main extends Application {
         VBox cb_box = new VBox(25);
         cb_box.getChildren().addAll(wifi_adapter_cb, wireless_network_cb, deauth_area);
         
-        // (VB) Start Button
+        // (HB) Start Button & Scan Button
         Button start_button = new Button("Start Attack");
         start_button.setFont(font1);
         start_button.setStyle("-fx-text-fill: #021526; -fx-background-color: white;");
-        start_button.setPrefWidth(400);
+        start_button.setPrefWidth(300);
         
-        VBox button_box = new VBox(start_button);
+        Button scan_button = new Button("Scan");
+        scan_button.setFont(font1);
+        scan_button.setStyle("-fx-text-fill: #021526; -fx-background-color: white;");
+        scan_button.setPrefWidth(300);
         
+        HBox button_box = new HBox(10);
+		button_box.getChildren().addAll(start_button, scan_button);
+		
         // Layout Arrangement
-        HBox panel_box = new HBox(5);
+        HBox panel_box = new HBox(20);
         panel_box.getChildren().addAll(label_box, cb_box);
         panel_box.setAlignment(javafx.geometry.Pos.CENTER);
         
@@ -131,7 +137,7 @@ public class Main extends Application {
         
         Scene scene = new Scene(root, 1050, 500);
         primaryStage.setTitle("PayDOS");
-        //primaryStage.setResizable(false);
+        primaryStage.setResizable(false);
         primaryStage.getIcons().add(new Image("src/icon.png"));
         primaryStage.setScene(scene);
         primaryStage.show();
